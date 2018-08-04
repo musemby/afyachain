d25e377b5e171b247b616757bca1336a38d84ced
musembi <musembinzioki@gmail.com>
2018-08-04 13:09:40 +0300

    Add github auth strategy using passport

---
+++ afyachain/afyachain-network/env.sh
@@ -0,0 +1,12 @@
+export COMPOSER_PROVIDERS='{
+  "github": {
+    "provider": "github",
+    "module": "passport-github",
+    "clientID": "b5b29e03df5f80d9dc82",
+    "clientSecret": "698e9c5250caef456f5a71e4c040207b57a7dce4",
+    "authPath": "/auth/github",
+    "callbackURL": "/auth/github/callback",
+    "successRedirect": "/",
+    "failureRedirect": "/"
+  }
+}'
