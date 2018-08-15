4c1e1b94d507d1333406d0a1f8138fa05fb09cfd
musembi <musembinzioki@gmail.com>
2018-08-16 01:26:52 +0300

    Fixed card crisis

--- afyachain/afyachain-network/start_network.sh
+++ afyachain/afyachain-network/start_network.sh
@@ -1,4 +1,4 @@
 cd ~/personal/leproject/afyachain/afyachain-network
 composer network install --card PeerAdmin@hlfv1 --archiveFile "afyachain-network@0.0.$1.bna"
-composer network start --networkName afyachain-network --networkVersion "0.0.$1" --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer network start --networkName afyachain-network --networkVersion "0.0.$1" --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file admin@afyachain-network
 composer network ping --card admin@afyachain-network
